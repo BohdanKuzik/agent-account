6bfeaeeabc947f270cd0e9ac4480ddcd455c6d93
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-15 21:20:49 +0200

    added admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Player, Agent, Club, Transfer
 
-# Register your models here.
+
+@admin.register(Agent)
+class AgentAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    search_fields = ("transfer_date",)
+    list_filter = ("transaction_amount",)
+
+
+admin.site.register(Club)
+
+admin.site.register(Player)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,57 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    country = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("last_name",)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Agent(AbstractUser):
+    players = models.ManyToManyField(Player, related_name="agents")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Club(models.Model):
+    club_name = models.CharField(max_length=255, unique=True)
+    league = models.CharField(max_length=255)
+    players = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        related_name="clubs"
+    )
+
+    class Meta:
+        ordering = ("club_name",)
+
+    def __str__(self):
+        return self.club_name
+
+
+class Transfer(models.Model):
+    transfer_date = models.DateField()
+    club = models.ForeignKey(
+        Club,
+        on_delete=models.CASCADE,
+        related_name="transfers"
+    )
+    agents = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="transfers")
+    transaction_amount = models.IntegerField()
+
+    class Meta:
+        ordering = ("transfer_date",)
+
+    def __str__(self):
+        return f"Transfer on {self.transfer_date} for {self.transaction_amount} amount"
