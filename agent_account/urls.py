da610c6f3faa04d7c73aaf1a1cc9833f9f0687d4
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-17 13:24:06 +0200

    added registration directory and files login logged_out

--- agent_account/urls.py
+++ agent_account/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from agent_account import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
