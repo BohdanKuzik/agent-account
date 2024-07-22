ca3d2171bd29850be82f8d0f5e20e8ec9109c6aa
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 12:32:44 +0200

    edit file setting.py and created new data base postgresql

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 import sys
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,6 +86,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
