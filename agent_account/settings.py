9539afbc8ae22fc952b90c52836d6c93f8382f41
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 14:48:45 +0200

    edit file settings.py

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
