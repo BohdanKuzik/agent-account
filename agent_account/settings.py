d422887d33297b6533fcb7eba6ee51fe4fe2df1b
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 15:05:11 +0200

    edit file settings.py

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'agent-service-jd32.onrender.com']
 
 
 # Application definition
