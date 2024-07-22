5d40b8dd874236837f9d070fe5407f544944e5c8
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 11:04:32 +0200

    update settings and added requirements.txt

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 import sys
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^wr--i41e+umapj&h6kp4!m@$4qsdl=wazp%5ob7ymph%y(kg'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-%^wr--i41e+umapj&h6kp4!m@$4qsdl=wazp%5ob7ymph%y(kg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
