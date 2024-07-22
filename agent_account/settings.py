cb0b7182d30fe295fe2b67af41a081f711ce1f58
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 13:32:28 +0200

    remove insecure django secret key for deployment

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-%^wr--i41e+umapj&h6kp4!m@$4qsdl=wazp%5ob7ymph%y(kg')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
