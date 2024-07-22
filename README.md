d444e6e261922ac3a9500264e1371122e4e0adbf
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-22 10:10:53 +0200

    add file for generate data

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
+# Agent Service
 
-# Agent Management System
+Web application for football agents to view various information about players on the market, and about transactions made by other clubs and agents
+
+## Check it out!
+
+[Agent Manager App]()
+<br>
+(You would need to wait for ~1 minute for the server to wake up)
+
+<i>If you don't want to register you can use the following credentials:</i>
+
+* <i>Login: admin25</i>
+* <i>Password: bodya1234</i>
+
+## Installing locally / Getting started
+
+Python3 must be already installed
+
+```shell
+git clone https://github.com/BohdanKuzik/agent-account.git
+cd agent-account
+python3 -m venv venv
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py loaddata generated_data.json
+python manage.py runserver
+```
+
+After running that code you'll be able to browse the website locally on your computer.
+
+### Features
+
+* Authentication functionality for Worker/User
+* Managing tasks and projects from the presented interface
+* Following tasks deadlines and assignees
+* Filtering and searching
+* Admin panel for advanced DB management
+
+## Demo
+![Website Interface](demo.png)
---
+++ catalog/management/commands/generator_data.py
@@ -0,0 +1,60 @@
+import os
+from random import randint, choice
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from faker import Faker
+from catalog.models import Player, Club, Transfer, Agent
+
+
+class Command(BaseCommand):
+    help = 'Generate random data for the database'
+
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+
+        players = []
+        for _ in range(10):
+            player = Player.objects.create(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                age=randint(16, 41),
+                country=fake.country(),
+                position=choice(['Forward', 'Midfielder', 'Defender', 'Goalkeeper'])
+            )
+            players.append(player)
+
+        agents = []
+        User = get_user_model()
+        for _ in range(5):
+            agent = User.objects.create_user(
+                username=fake.user_name(),
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.email(),
+                password='password123'
+            )
+            if isinstance(agent, Agent):
+                assigned_players = fake.random_elements(elements=players, length=randint(1, 3), unique=True)
+                for player in assigned_players:
+                    player.agent = agent
+                    player.save()
+            agents.append(agent)
+
+        clubs = []
+        for _ in range(5):
+            club = Club.objects.create(
+                club_name=fake.company(),
+                league=fake.word(),
+            )
+            clubs.append(club)
+
+        for _ in range(10):
+            transfer = Transfer.objects.create(
+                transfer_date=fake.date_between(start_date='-2y', end_date='today'),
+                club=choice(clubs),
+                transaction_amount=randint(50000, 300000000)
+            )
+            random_agents = fake.random_elements(elements=agents, length=randint(1, 3), unique=True)
+            transfer.agents.add(*random_agents)
+
+        self.stdout.write(self.style.SUCCESS("Data generation completed!"))
