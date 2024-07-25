a7c63f55725ed4801a5868f28e18ec1fc6a0798b
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-07-25 17:11:58 +0200

    fix gitignore

--- agent_account/settings.py
+++ agent_account/settings.py
@@ -85,14 +85,11 @@ WSGI_APPLICATION = 'agent_account.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=500
+    )
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
